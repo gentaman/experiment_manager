617794844ff259470110b9b38f8a070241cb522d
gentaman <gentaman01@gmail.com>
2018-07-09 22:24:32 +0900

    update batch manager

--- src/BatchMan.py
+++ src/BatchMan.py
@@ -1,19 +1,44 @@
+import os
 
 class BatchMan():
     """
         Batch Manager
     """
-    def __init__(self):
+    def __init__(self, dbman, storageman=None, main_algorithm=None):
+        self.dbman = dbman
+        self.storageman = dbman
+        self.main_algorithm = main_algorithm
+
+    def setup(self):
         pass
 
-    def do_experiment(self):
+    def do_experiment(self, main_algorithm, config):
         pass
 
-    def get_result(self):
-        pass
+    def do_each_experiment(self, config, main_algorithm=None):
+        if main_algorithm is not None:
+            main_algorithm(**config)
+        else:
+            self.main_algorithm(**config)
+        result = self.get_result(config)
+        self.store_result(result)
+        self.dbman.set_experimentinfo(config)
+        self.dbman.record()
 
-    def store_result(self):
-        pass
+    def get_result(self, config):
+        path = config["path"]
+        fname= config["experimetinfo_file"]
+        with open(os.path.join(path, fname)) as f:
+            res = f.read()
+        return res
 
-    def record(self):
+    def store_result(self, result):
+        if self.storageman is None:
+            self.dbman.set_storeinfo(result)
+        else:
+            # TODO:
+            self.storageman.store_result(result)
+
+
+    def record(self, config):
         pass
