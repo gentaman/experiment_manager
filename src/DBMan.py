bc268ba0850cd696faacac8e9873b51360810862
gentaman <gentaman01@gmail.com>
2018-07-09 22:24:52 +0900

    update database manager

--- src/DBMan.py
+++ src/DBMan.py
@@ -3,7 +3,13 @@ class DBMan():
     """
         Database Manager
     """
-    def __init__(self):
+    def __init__(self, db_info):
+        self.db_info = db_info
+
+    def set_storeinfo(self, result):
+        pass
+
+    def set_experimentinfo(self, config):
         pass
 
     def record(self):
